fc92b31758b0ee6392fef54ed192112e5263606e
ThalesSilva19 <thales.wds2019@gmail.com>
2022-10-31 23:25:11 -0300

    Navbar created

---
+++ src/components/navbar.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+
+import { Toolbar, Typography } from '@mui/material'
+
+import Link from 'next/link'
+
+export default function Navbar() {
+    return (
+        <Toolbar
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: '100%',
+                minHeight: '0px !important',
+                padding: '8px',
+                backgroundColor: '#FFB600',
+
+            }}
+        >
+            <Link href="/">
+                <Typography sx={{
+                    padding: '4px 8px 4px 8px',
+                    borderLeft: 'solid 1px #000',
+                    
+                    "&:hover": {
+                        cursor: 'pointer !important',
+                        textDecoration: 'underline',
+                    },
+                }}>Cervejas</Typography>
+            </Link>
+            <Link href="/">
+                <Typography sx={{
+                    padding: '4px 8px 4px 8px',
+                    borderLeft: 'solid 1px #000',
+
+                    "&:hover": {
+                        cursor: 'pointer !important',
+                        textDecoration: 'underline',
+                    },
+                }}>Kits</Typography>
+            </Link>
+            <Link href="/">
+                <Typography sx={{
+                    padding: '4px 8px 4px 8px',
+                    borderLeft: 'solid 1px #000',
+
+                    "&:hover": {
+                        cursor: 'pointer !important',
+                        textDecoration: 'underline',
+                    },
+                }}>Marcas</Typography>
+            </Link>
+            <Link href="/">
+                <Typography sx={{
+                    padding: '4px 8px 4px 8px',
+                    borderLeft: 'solid 1px #000',
+
+                    "&:hover": {
+                        cursor: 'pointer !important',
+                        textDecoration: 'underline',
+                    },
+                }}>Outras Bebidas</Typography>
+            </Link>
+            <Link href="/">
+                <Typography sx={{
+                    padding: '4px 8px 4px 8px',
+                    borderLeft: 'solid 1px #000',
+
+                    "&:hover": {
+                        cursor: 'pointer !important',
+                        textDecoration: 'underline',
+                    },
+                }}>Promoções</Typography>
+            </Link>
+            <Link href="/">
+                <Typography sx={{
+                    padding: '4px 8px 4px 8px',
+                    borderLeft: 'solid 1px #000',
+                    borderRight: 'solid 1px #000',
+
+                    "&:hover": {
+                        cursor: 'pointer !important',
+                        textDecoration: 'underline',
+                    },
+                }}>Presentes</Typography>
+            </Link>
+        </Toolbar>
+    )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ProductCard from '../components/product_card'
 import Footer from '../components/footer'
+import Navbar from '../components/navbar'
 
 export default function index() {
   return (
     <main>
+      <Navbar/>
       <ProductCard/>
       <Footer/>
     </main>
