72bf1d58be445a8eca26268c1f638b4079409e79
Vinícius Silva Fernandes Kuhlmann <vinikuhlmann@gmail.com>
2022-12-07 16:02:20 -0300

    Validação de formulários implementada

--- src/components/login_card.jsx
+++ src/components/login_card.jsx
@@ -5,86 +5,85 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 export default function LoginCard() {
 
-    return (
+  return (
 
 
-        <Card sx={{
-            width: '50%',
-            padding: '64px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            textAlign: "center",
-            justifyContent: 'center',
-            gap: '32px'
+    <Card sx={{
+      width: '50%',
+      padding: '64px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      textAlign: "center",
+      justifyContent: 'center',
+      gap: '32px'
+    }}>
+      <Typography variant='h4' tabIndex='0'> Acesse sua conta </Typography>
+
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={values => {
+          const errors = {};
+          if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+            errors.email = 'Digite um e-mail válido; Ex: pessoa@exemplo.com';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
         }}>
-            <Typography variant='h4' tabIndex='0'> Acesse sua conta </Typography>
-            <Formik
-                initialValues={{ email: '', password: '' }}
-                validate={values => {
-                    const errors = {};
-                    if (!values.email) {
-                        errors.email = 'Digite um e-mail válido; Ex: fulano@exemplo.com';
-                    } else if (
-                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                    ) {
-                        errors.email = 'Invalid email address';
-                    }
-                    return errors;
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2));
-                        setSubmitting(false);
-                    }, 400);
-                }}
-            >
-                {({ isSubmitting }) => (
-                    <Form>
-                        <Box sx={{
-                            display: 'flex',
-                            justifyContent: 'space-evenly',
-                            flexWrap: 'wrap',
-                            padding: '12px'
-                        }}>
-                            <TextField required label="E-mail" name="email" variant="outlined" size="small" sx={{ width: '80%' }}></TextField>
-                            <ErrorMessage type="email" name="email" component="div" />
-                        </Box>
-                        <Box sx={{
-                            display: 'flex',
-                            justifyContent: 'space-evenly',
-                            flexWrap: 'wrap',
-                            padding: '12px'
-                        }}>
-                            <TextField required label="Senha" name="password" type="password" variant="outlined" size="small" sx={{ width: '80%' }}></TextField>
-                            <ErrorMessage name="password" component="div" />
-                        </Box>
-                        <Button variant='contained' disabled={isSubmitting} type="submit" sx={{
-                            backgroundColor: '#FFB600',
-                            color: '#000000',
-                            width: '80%',
-                            height: '32px',
-                            margin: '12px'
-                        }}>
-                            <Typography variant='h6'>Continuar</Typography>
-                        </Button>
-                        <Link href='/auth/register'>
-                            <Button variant='contained'
-                                sx={{
-                                    backgroundColor: '#EFEFEF',
-                                    color: '#000000',
-                                    width: '80%',
-                                    height: '32px',
-                                    margin: '12px'
-                                }}
-                            >
-                                <Typography variant='h6'>Criar Conta</Typography>
-                            </Button>
-                        </Link>
-                    </Form>
-                )}
-            </Formik>
 
-        </Card>
-    )
+        {({ isSubmitting }) => (
+          <Form>
+            <Box sx={{
+              display: 'flex',
+              justifyContent: 'space-evenly',
+              flexWrap: 'wrap',
+              padding: '12px'
+            }}>
+              <TextField required label="E-mail" name="email" variant="outlined" size="small" sx={{ width: '80%' }}></TextField>
+              <ErrorMessage name="email" component="div" />
+            </Box>
+
+            <Box sx={{
+              display: 'flex',
+              justifyContent: 'space-evenly',
+              flexWrap: 'wrap',
+              padding: '12px'
+            }}>
+              <TextField required label="Senha" name="password" variant="outlined" size="small" sx={{ width: '80%' }}></TextField>
+              <ErrorMessage name="password" component="div" />
+            </Box>
+
+            <Button variant='contained' disabled={isSubmitting} type="submit" sx={{
+              backgroundColor: '#FFB600',
+              color: '#000000',
+              width: '80%',
+              height: '32px',
+              margin: '12px'
+            }}>
+              <Typography variant='h6'>Continuar</Typography>
+            </Button>
+
+            <Link href='/auth/register'>
+              <Button variant='contained'
+                sx={{
+                  backgroundColor: '#EFEFEF',
+                  color: '#000000',
+                  width: '80%',
+                  height: '32px',
+                  margin: '12px'
+                }}>
+                <Typography variant='h6'>Criar Conta</Typography>
+              </Button>
+            </Link>
+          </Form>
+        )}
+      </Formik>
+
+    </Card>
+  )
 }
