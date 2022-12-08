c5189ecbe0a801664ea8cfaa68e190d8057c8137
victorkendi <kendi_victor@usp.br>
2022-12-07 22:54:35 -0300

    login e register

--- src/components/login_card.jsx
+++ src/components/login_card.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
-import Link from 'next/link'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import * as yup from 'yup';
 import { Card, Typography, TextField, Button, Box } from '@mui/material';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useFormik } from 'formik';
+
+const validationSchema = yup.object({
+  email: yup
+    .string('Digite seu email')
+    .email('Digite um email válido')
+    .required('Email é obrigatório'),
+  password: yup
+    .string('Digite sua senha')
+    .min(8, 'Senha deve ter pelo menos 8 caracteres')
+    .required('Senha é obrigatória'),
+});
 
 export default function LoginCard() {
+  const router = useRouter();
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: () => {
+      router.push('/');
+    },
+  })
 
   return (
-
 
     <Card sx={{
       width: '50%',
@@ -20,69 +43,71 @@ export default function LoginCard() {
     }}>
       <Typography variant='h4' tabIndex='0'> Acesse sua conta </Typography>
 
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        validate={values => {
-          const errors = {};
-          if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-            errors.email = 'Digite um e-mail válido; Ex: pessoa@exemplo.com';
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
+      <form onSubmit={formik.handleSubmit}>
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+          padding: '12px'
         }}>
+          <TextField
+            required
+            label="E-mail"
+            name="email"
+            variant="outlined"
+            size="small"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+            sx={{ width: '80%' }}>
+          </TextField>
+        </Box>
 
-        {({ isSubmitting }) => (
-          <Form>
-            <Box sx={{
-              display: 'flex',
-              justifyContent: 'space-evenly',
-              flexWrap: 'wrap',
-              padding: '12px'
-            }}>
-              <TextField required label="E-mail" name="email" variant="outlined" size="small" sx={{ width: '80%' }}></TextField>
-              <ErrorMessage name="email" component="div" />
-            </Box>
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+          padding: '12px'
+        }}>
+          <TextField
+            required
+            label="Senha"
+            name="password"
+            variant="outlined"
+            size="small"
+            type="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+            sx={{ width: '80%' }}>
+          </TextField>
+        </Box>
 
-            <Box sx={{
-              display: 'flex',
-              justifyContent: 'space-evenly',
-              flexWrap: 'wrap',
-              padding: '12px'
-            }}>
-              <TextField required label="Senha" name="password" variant="outlined" size="small" sx={{ width: '80%' }}></TextField>
-              <ErrorMessage name="password" component="div" />
-            </Box>
+        <Button variant='contained' type="submit" sx={{
+          backgroundColor: '#FFB600',
+          color: '#000000',
+          width: '80%',
+          height: '32px',
+          margin: '12px'
+        }}>
+          <Typography variant='h5' sx={{fontSize:'1.25em'}}>Continuar</Typography>
+        </Button>
 
-            <Button variant='contained' disabled={isSubmitting} type="submit" sx={{
-              backgroundColor: '#FFB600',
+        <Link href='/auth/register'>
+          <Button variant='contained'
+            sx={{
+              backgroundColor: '#EFEFEF',
               color: '#000000',
               width: '80%',
-              height: '32px',
+              height: 'auto',
               margin: '12px'
             }}>
-              <Typography variant='h5' sx={{fontSize:'1.5em'}}>Continuar</Typography>
-            </Button>
-
-            <Link href='/auth/register'>
-              <Button variant='contained'
-                sx={{
-                  backgroundColor: '#EFEFEF',
-                  color: '#000000',
-                  width: '80%',
-                  height: 'auto',
-                  margin: '12px'
-                }}>
-                <Typography variant='h5' sx={{fontSize:'1.5em'}}>Criar Conta</Typography>
-              </Button>
-            </Link>
-          </Form>
-        )}
-      </Formik>
+            <Typography variant='h5' sx={{fontSize:'1.25em'}}>Criar Conta</Typography>
+          </Button>
+        </Link>
+      </form>
 
     </Card>
   )
--- src/components/register_card.jsx
+++ src/components/register_card.jsx
@@ -1,162 +1,212 @@
 import React from 'react'
+import { useRouter } from 'next/router' 
 import Link from 'next/link'
-import dayjs, { Dayjs } from 'dayjs';
+import * as yup from 'yup';
+import "yup-phone-lite";
+import  {cpf as cpf1} from "cpf-cnpj-validator";
 import { Box, Card, Typography, TextField, Button, FormLabel, FormControlLabel, RadioGroup, Radio } from '@mui/material';
-import { Formik, Form, ErrorMessage } from 'formik';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { useFormik } from 'formik';
+
+const validationSchema = yup.object({
+  email: yup
+    .string('Digite seu Nome Completo')
+    .required('Nome Completo é obrigatório'),
+  email: yup
+    .string('Digite seu email')
+    .email('Digite um email válido')
+    .required('Email é obrigatório'),
+  cpf: yup
+    .string('Digite o CPF')
+    .required('CPF é obrigatório')
+    .test("aaa", value => cpf1.isValid(value)),
+  phonenumber: yup
+    .string('Digite um telefone')
+    .phone("BR", "Digite um número de telefone válido")
+    .required('Telefone é obrigatório'),
+  password: yup
+    .string('Digite sua senha')
+    .min(8, 'Senha deve ter pelo menos 8 caracteres')
+    .required('Senha é obrigatória'),
+});
 
 export default function RegisterCard() {
-  const [value, setValue] = React.useState(
-    dayjs('2022-12-06T16:00:00')
-  );
-
-  const handleChange = (newValue) => {
-    setValue(newValue);
-  };
+  const router = useRouter();
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      cpf: '',
+      phonenumber: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      router.push('/auth/login')
+    },
+  })
 
   return (
     <Card sx={{
       width: '50%',
-      padding: '64px',
+      padding: 'calc((8px + 1.5625vw)*2)',
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
       textAlign: "center",
       justifyContent: 'center',
-      gap: '32px'
+      gap: 'calc(8px + 1.5625vw)'
     }}>
       <Typography variant='h4' tabIndex='0'> Crie sua conta </Typography>
 
-      <Formik
-        initialValues={{ name: '', cpf: '', phone: '', email: '', password: '' }}
-        validate={values => {
-          const errors = {};
-
-          const email_regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/;
-          if (!email_regex.test(values.email)) {
-            errors.email = 'Digite um e-mail válido; Ex: pessoa@exemplo.com';
-          }
-
-          const cpf_regex = /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/;
-          if (!cpf_regex.test(values.cpf)) {
-            errors.cpf = 'Digite um CPF válido; Ex: 123.456.789-10';
-          }
-
-          const phone_regex = /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/;
-          if (!phone_regex.test(values.phone)) {
-            errors.phone = 'Digite um telefone válido; Ex: 11912345678';
-          }
-
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
+      <form onSubmit={formik.handleSubmit}>
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+          padding: 'calc((8px + 1.5625vw)/2)'
         }}>
+          <TextField
+            required
+            label='Nome Completo'
+            name='name'
+            variant="outlined"
+            size="small"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            error={formik.touched.name && Boolean(formik.errors.name)}
+            helperText={formik.touched.name && formik.errors.name}
+            sx={{ width: '98%' }}>
+          </TextField>
+        </Box>
 
-        {({ isSubmitting }) => (
-          <Form>
-            <Box sx={{
-              display: 'flex',
-              justifyContent: 'space-evenly',
-              flexWrap: 'wrap',
-              padding: '12px'
-            }}>
-              <TextField required label='Nome Completo' variant="outlined" size="small" sx={{ width: '98%' }}></TextField>
-            </Box>
-            <Box sx={{
-              display: 'flex',
-              justifyContent: 'space-evenly',
-              flexWrap: 'wrap',
-              padding: '12px',
-              alignItems: 'center',
-            }}>
-              <Box>
-                <FormLabel id="demo-radio-buttons-group-label" tabIndex='0'>Gênero</FormLabel>
-                <RadioGroup
-                  aria-labelledby="demo-radio-buttons-group-label"
-                  defaultValue="female"
-                  name="radio-buttons-group"
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'row'
-                  }}
-                >
-                  <FormControlLabel value="female" tabIndex='0' control={<Radio />} label="Feminino" />
-                  <FormControlLabel value="male" tabIndex='0' control={<Radio />} label="Masculino" />
-                  <FormControlLabel value="other" tabIndex='0' control={<Radio />} label="Não Responder" />
-                </RadioGroup>
-              </Box>
-            </Box>
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+          padding: 'calc((8px + 1.5625vw)/2)',
+          alignItems: 'center',
+        }}>
+          <Box>
+            <FormLabel id="demo-radio-buttons-group-label" tabIndex='0'>Gênero</FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="female"
+              name="radio-buttons-group"
+              sx={{
+                display: 'flex',
+                flexDirection: 'row'
+              }}
+            >
+              <FormControlLabel value="female" tabIndex='0' control={<Radio />} label="Feminino" />
+              <FormControlLabel value="male" tabIndex='0' control={<Radio />} label="Masculino" />
+              <FormControlLabel value="other" tabIndex='0' control={<Radio />} label="Não Responder" />
+            </RadioGroup>
+          </Box>
+        </Box>
 
-            <Box sx={{
-              display: 'flex',
-              justifyContent: 'space-evenly',
-              flexWrap: 'wrap',
-              padding: '12px'
-            }}>
-              <TextField required label='CPF' name="cpf" variant="outlined" size="small" sx={{ width: '98%' }}></TextField>
-              <ErrorMessage name="cpf" component="div" />
-            </Box>
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+          padding: 'calc((8px + 1.5625vw)/2)'
+        }}>
+          <TextField
+            required
+            label='CPF'
+            name="cpf"
+            variant="outlined"
+            size="small"
+            value={formik.values.cpf}
+            onChange={formik.handleChange}
+            error={formik.touched.cpf && Boolean(formik.errors.cpf)}
+            helperText={formik.touched.cpf && formik.errors.cpf}
+            sx={{ width: '98%' }}> 
+          </TextField>
+        </Box>
+        
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+          padding: 'calc((8px + 1.5625vw)/2)'
+        }}>
+          <TextField
+            required
+            label='Telefone'
+            name="phonenumber"
+            variant="outlined"
+            size="small"
+            value={formik.values.phonenumber}
+            onChange={formik.handleChange}
+            error={formik.touched.phonenumber && Boolean(formik.errors.phonenumber)}
+            helperText={formik.touched.phonenumber && formik.errors.phonenumber}
+            sx={{ width: '98%' }}>
+          </TextField>
+        </Box>
 
-            <Box sx={{
-              display: 'flex',
-              justifyContent: 'space-evenly',
-              flexWrap: 'wrap',
-              padding: '12px'
-            }}>
-              <TextField required label='Telefone' name="phone" variant="outlined" size="small" sx={{ width: '98%' }}></TextField>
-              <ErrorMessage name="phone" component="div" />
-            </Box>
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+          padding: 'calc((8px + 1.5625vw)/2)'
+        }}>
+          <TextField
+            required
+            label="E-mail"
+            name="email"
+            variant="outlined"
+            size="small"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+            sx={{ width: '98%' }}>
+          </TextField>
+        </Box>
 
-            <Box sx={{
-              display: 'flex',
-              justifyContent: 'space-evenly',
-              flexWrap: 'wrap',
-              padding: '12px'
-            }}>
-              <TextField required label='E-mail' name="email" variant="outlined" size="small" sx={{ width: '98%' }}></TextField>
-              <ErrorMessage name="email" component="div" />
-            </Box>
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+          padding: 'calc((8px + 1.5625vw)/2)'
+        }}>
+          <TextField
+            required
+            label="Senha"
+            name="password"
+            variant="outlined"
+            size="small"
+            type="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+            sx={{ width: '98%' }}>
+          </TextField>
+        </Box>
+        <Button variant='contained' type="submit" sx={{
+          backgroundColor: '#FFB600',
+          color: '#000000',
+          width: '80%',
+          height: 'auto',
+          margin: 'calc((8px + 1.5625vw)/2)'
+        }}>
+          <Typography variant='h5' sx={{fontSize: '1.25em'}}>Continuar</Typography>
+        </Button>
 
-            <Box sx={{
-              display: 'flex',
-              justifyContent: 'space-evenly',
-              flexWrap: 'wrap',
-              padding: '12px'
-            }}>
-              <TextField required label='Senha' name="password" variant="outlined" size="small" sx={{ width: '98%' }}></TextField>
-            </Box>
-
-            <Button variant='contained' disabled={isSubmitting} type="submit" sx={{
-              backgroundColor: '#FFB600',
+        <Link href='/auth/login'>
+          <Button variant='contained'
+            sx={{
+              backgroundColor: '#EFEFEF',
               color: '#000000',
               width: '80%',
-              height: '32px',
-              margin: '12px'
+              height: 'auto',
+              margin: 'calc((8px + 1.5625vw)/2)'
             }}>
-              <Typography variant='h6'>Continuar</Typography>
-            </Button>
-
-            <Link href='/auth/register'>
-              <Button variant='contained'
-                sx={{
-                  backgroundColor: '#EFEFEF',
-                  color: '#000000',
-                  width: '80%',
-                  height: '32px',
-                  margin: '12px'
-                }}>
-                <Typography variant='h6'>Acessar a conta</Typography>
-              </Button>
-            </Link>
-          </Form>
-        )}
-      </Formik>
+            <Typography variant='h5' sx={{fontSize: '1.25em'}}>Acessar a conta</Typography>
+          </Button>
+        </Link>
+      </form>
     </Card>
   )
 }
